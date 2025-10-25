aad390bdf446c8619031501847fa6767ef97bfa3
AishaSaleem13 <aishasaleem1308@gmail.com>
2025-10-25 07:43:20 +0500

    done

--- api/server.mjs
+++ api/server.mjs
@@ -7,4 +7,4 @@ db.connection.once("open", () => console.log("✅ Connected to DB"))
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
