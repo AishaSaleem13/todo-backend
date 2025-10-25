a0e445950eb71ec60ef5f7c37ad05ba33f372bb3
AishaSaleem13 <aishasaleem1308@gmail.com>
2025-10-25 15:31:57 +0500

    fixed

--- api/index.mjs
+++ api/index.mjs
@@ -1,11 +1,11 @@
 import express from "express";
 
 import cors from "cors";
-import taskRoutes from "./routes/task.mjs";
+import router from "./routes/mainindex.mjs";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.get("/", (req, res) => res.send("Backend is running"));
-app.use("/api/tasks", taskRoutes);
+app.use("/", router);
  export default app
--- api/server.mjs
+++ api/server.mjs
@@ -8,3 +8,4 @@ db.connection.once("open", () => console.log("✅ Connected to DB"))
 const PORT = process.env.PORT || 5000;
 
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app
