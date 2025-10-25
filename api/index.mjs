0abce031228548da045d1d9cb4c9007397315387
AishaSaleem13 <aishasaleem1308@gmail.com>
2025-10-25 07:41:15 +0500

    done

--- api/index.mjs
+++ api/index.mjs
@@ -1,14 +1,11 @@
 import express from "express";
 
 import cors from "cors";
-import router from "./routes/mainindex.mjs";
+import taskRoutes from "./routes/task.mjs";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.get("/", (req, res) => {
-  res.send("its running");
-});
-
- app.use("/",router)
+app.get("/", (req, res) => res.send("Backend is running"));
+app.use("/api/tasks", taskRoutes);
  export default app
--- api/server.mjs
+++ api/server.mjs
@@ -1,11 +1,10 @@
+
 import db from "./config/db.mjs";
-import app from "./index.mjs"
+import app from "./index.mjs";
+
 db.connection.once("open", () => console.log("✅ Connected to DB"))
   .on("error", (err) => console.log("❌ DB connection error -->", err));
 
+const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== "production") {
-  app.listen(5000, () => {
-    console.log("Server is running on port ");
-  });
-}
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
