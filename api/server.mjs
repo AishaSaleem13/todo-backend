99adc07872ee5913a010fb1118cd10a4afc5fbf8
AishaSaleem13 <aishasaleem1308@gmail.com>
2025-11-03 01:54:45 +0500

    remove listen

--- api/server.mjs
+++ api/server.mjs
@@ -7,5 +7,5 @@ db.connection.once("open", () => console.log("✅ Connected to DB"))
 
 const PORT = process.env.PORT || 5000;
 
- app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 export default app
